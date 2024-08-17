a8d9041e8ca10af26943e81595e083f0124f6a3e
Heesho <hisham11027@gmail.com>
2024-08-16 22:31:00 -0700

    cleanin up

--- contracts/TouchBadge.sol
+++ contracts/TouchBadge.sol
@@ -20,8 +20,10 @@ contract TouchBadge is ERC1155, Ownable, EIP712 {
     uint256 public constant BURN_RATE = 50;
     uint256 public constant DIVISOR = 100;
 
-    address public immutable touchToken;
-    uint256 public rewardAmount = 1 ether;
+    address public touchToken;
+    uint256 public touchReward = 1 ether;
+    uint256 public creatorReward = 1 ether;
+    uint256 public ownerReward = 1 ether;
 
     struct ECDSASignature {
         bytes32 r;
@@ -47,6 +49,11 @@ contract TouchBadge is ERC1155, Ownable, EIP712 {
     event TouchBadge__Registered(uint256 indexed tokenId, address owner, address touch);
     event TouchBadge__Touched(uint256 indexed tokenId, address account);
     event TouchBadge__Stolen(uint256 indexed tokenId, address newOwner, address prevOwner, uint256 price);
+    event TouchBadge__TouchDataSet(uint256 indexed tokenId, string touchName, string touchUri);
+    event TouchBadge__TouchTokenSet(address touchToken_);
+    event TouchBadge__TouchRewardSet(uint256 touchReward_);
+    event TouchBadge__CreatorRewardSet(uint256 creatorReward_);
+    event TouchBadge__OwnerRewardSet(uint256 ownerReward_);
 
     error TouchBadge__InvalidId();
     error TouchBadge__Unauthorized();
@@ -54,25 +61,27 @@ contract TouchBadge is ERC1155, Ownable, EIP712 {
     error TouchBadge__AlreadyRegistered();
     error TouchBadge__InvalidNonce();
 
-    constructor(address _touchToken) 
+    constructor(address touchToken_) 
         ERC1155("") 
         EIP712("TouchBadge", "1") 
     {
-        touchToken = _touchToken;
+        touchToken = touchToken_;
     }
 
     function register(
         address owner,
-        address touch,
+        address touch_,
         string memory touchName,
         string memory touchUri
     ) external {
-        if (touch_TokenId[touch] != 0) revert TouchBadge__AlreadyRegistered();
+        if (touch_TokenId[touch_] != 0) revert TouchBadge__AlreadyRegistered();
+
         currentIndex++;
-        TouchData memory touchData = TouchData(touch, owner, owner, 0, touchName, touchUri);
+        TouchData memory touchData = TouchData(touch_, owner, owner, 0, touchName, touchUri);
         tokenId_TouchData[currentIndex] = touchData;
-        touch_TokenId[touch] = currentIndex;
-        emit TouchBadge__Registered(currentIndex, owner, touch);
+        touch_TokenId[touch_] = currentIndex;
+
+        emit TouchBadge__Registered(currentIndex, owner, touch_);
     }
 
     function touch(
@@ -80,7 +89,6 @@ contract TouchBadge is ERC1155, Ownable, EIP712 {
         ECDSASignature calldata sig
     ) external {
         uint256 nonce = account_Nonce[account];
-
         bytes32 digest = _hashTypedDataV4(
             keccak256(
                 abi.encode(TOUCH_TYPEHASH, account, nonce)
@@ -88,16 +96,19 @@ contract TouchBadge is ERC1155, Ownable, EIP712 {
         );
         address touched = ECDSA.recover(digest, sig.v, sig.r, sig.s);
         uint256 tokenId = touch_TokenId[touched];
+
         if (tokenId == 0) revert TouchBadge__Unauthorized();
         if (tokenId_Account_Timestamp[tokenId][account] + DURATION >= block.timestamp) revert TouchBadge__AlreadyTouched();
         
         account_Nonce[account]++;
         tokenId_Account_Timestamp[tokenId][account] = block.timestamp;
         _mint(account, tokenId, 1, "");
+
         emit TouchBadge__Touched(tokenId, account);
-        ITouchToken(touchToken).mint(tokenId_TouchData[tokenId].creator, rewardAmount);
-        ITouchToken(touchToken).mint(tokenId_TouchData[tokenId].owner, rewardAmount);
-        ITouchToken(touchToken).mint(account, rewardAmount);
+
+        ITouchToken(touchToken).mint(tokenId_TouchData[tokenId].creator, creatorReward);
+        ITouchToken(touchToken).mint(tokenId_TouchData[tokenId].owner, ownerReward);
+        ITouchToken(touchToken).mint(account, touchReward);
     }
 
     function steal(
@@ -116,13 +127,14 @@ contract TouchBadge is ERC1155, Ownable, EIP712 {
         uint256 burnAmount = surplus * 50 / 100;
         uint256 prevOwnerAmount = prevPrice + burnAmount;
         tokenId_TouchData[tokenId] = touchCache;
+
         emit TouchBadge__Stolen(tokenId, msg.sender, prevOwner, touchCache.price);
 
         ITouchToken(touchToken).burn(msg.sender, touchCache.price);
         ITouchToken(touchToken).mint(prevOwner, prevOwnerAmount);
     }
 
-    function updateTouchData(
+    function setTouchData(
         uint256 tokenId,
         string memory touchName,
         string memory touchUri
@@ -130,6 +142,40 @@ contract TouchBadge is ERC1155, Ownable, EIP712 {
         if (msg.sender != tokenId_TouchData[tokenId].creator) revert TouchBadge__Unauthorized();
         tokenId_TouchData[tokenId].name = touchName;
         tokenId_TouchData[tokenId].uri = touchUri;
+
+        emit TouchBadge__TouchDataSet(tokenId, touchName, touchUri);
+    }
+
+    function setTouchToken(
+        address touchToken_
+    ) external onlyOwner {
+        touchToken = touchToken_;
+
+        emit TouchBadge__TouchTokenSet(touchToken_);
+    }
+
+    function setTouchReward(
+        uint256 touchReward_
+    ) external onlyOwner {
+        touchReward = touchReward_;
+
+        emit TouchBadge__TouchRewardSet(touchReward_);
+    }
+
+    function setCreatorReward(
+        uint256 creatorReward_
+    ) external onlyOwner {
+        creatorReward = creatorReward_;
+
+        emit TouchBadge__CreatorRewardSet(creatorReward_);
+    }
+
+    function setOwnerReward(
+        uint256 ownerReward_
+    ) external onlyOwner {
+        ownerReward = ownerReward_;
+
+        emit TouchBadge__OwnerRewardSet(ownerReward_);
     }
 
     function uri(
